566951484e8fee219a2e754e9074a04a66805630
sendelba26 <sendelba26@up.edu>
2023-09-05 13:59:38 -0700

    Revert "Revert "I made a small change""
    This reverts commit c0d9d8e6a8c1275a6f78defdc6170cb1863ede5a.

---
+++ app/src/main/java/com/example/olivialab1802923/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.olivialab1802923;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //hello
+    }
+}
